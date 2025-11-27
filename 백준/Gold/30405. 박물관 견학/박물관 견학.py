f13fbd24a4f93ca5e024e7ac0b22f92f559899c3
Park junhyeok <maple0118@dgu.ac.kr>
2025-11-27 22:16:40 +0900

    [Gold V] Title: 박물관 견학, Time: 440 ms, Memory: 145068 KB -BaekjoonHub

--- 백준/Gold/30405. 박물관 견학/박물관 견학.py
+++ 백준/Gold/30405. 박물관 견학/박물관 견학.py
@@ -4,6 +4,10 @@ def input():return sys.stdin.readline().rstrip()
 a,b = map(int,input().split())
 
 mus = []
+if a==1:
+    x,*k = map(int,input().split())
+    print(min(k[0],k[-1]))
+
 for i in range(a):
     x,*k = map(int,input().split())
     mus.append(k[0])
